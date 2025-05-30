91c5f12815b6c7e10a4a16f16095431a7940bbe2
Yariangel Aray <gabrielamunar4@gmail.com>
2025-05-30 11:01:58 -0500

    contador mensajes

--- src/acciones.js
+++ src/acciones.js
@@ -1,14 +1,45 @@
+import Swal from 'sweetalert2'
+// import 'animate.css';
+
+const mostrarMensajeError = (mensaje) => {
+  Swal.fire({
+    title: 'Error!',
+    // showClass: {
+    //   popup: `
+    //     animate__animated
+    //     animate__fadeInUp
+    //     animate__faster
+    //   `
+    // },
+    // hideClass: {
+    //   popup: `
+    //     animate__animated
+    //     animate__fadeOutDown
+    //     animate__faster
+    //   `
+    // },
+    text: mensaje,
+    icon: 'error',
+    confirmButtonText: 'Ok'
+  })
+}
+
 let contador = 0;
 
 export const sumar = (elemento) => {
-  if (contador >= 10) return;
-  
+  if (contador >= 10) {
+    mostrarMensajeError("No puedes seguir sumando.")
+    return;
+  }
   contador++;
   elemento.textContent = "Contador " + contador;
 }
 
 export const restar = (elemento) => {
-  if (contador <= 0) return;
+  if (contador <= 0) {
+    mostrarMensajeError("No puedes seguir restando.")
+    return;
+  }
   
   contador--;
   elemento.textContent = "Contador " + contador;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { sumar, restar, restablecer } from "./acciones";
 import "./style.css";
+import 'sweetalert2/src/sweetalert2.scss'
 
 const app = document.querySelector('#app');
 
@@ -10,27 +11,31 @@ span.classList.add("contador");
 const botonMas = document.createElement('button');
 botonMas.textContent = "+";
 botonMas.classList.add("boton");
+botonMas.classList.add("mas");
 
 const botonMenos = document.createElement('button');
 botonMenos.textContent = "-";
 botonMenos.classList.add("boton");
+botonMenos.classList.add("menos");
 
 const botonRestablecer = document.createElement('button');
 botonRestablecer.textContent = "Restablecer";
 botonRestablecer.classList.add("boton");
+botonRestablecer.classList.add("restaurar");
 
 const div = document.createElement('div');
 div.append(botonMas, botonMenos, botonRestablecer)
 app.append(span, div);
 
-botonMas.addEventListener('click', () => {
-  sumar(span);
-});
 
-botonMenos.addEventListener('click', () => {
-  restar(span);
-});
-
-botonRestablecer.addEventListener('click', () => {
-  restablecer(span);
-});
+document.addEventListener('click', (event) => {
+  if (event.target.closest('.mas')) {
+    sumar(span);
+  }
+  if (event.target.closest('.menos')) {
+    restar(span);
+  }
+  if (event.target.closest('.restaurar')) {
+    restablecer(span);
+  }
+})
